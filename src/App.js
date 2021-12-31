d907facd57e3163d4bf5bf1ab0bba3ec79d82cd9
hitesh911 <72139864+hitesh911@users.noreply.github.com>
2021-12-31 15:58:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -173,7 +173,9 @@ class App extends Component {
                             <div className="image card-body ">
                               <img href="#" src={element.image} />
                               <iframe  id = "myiframe"src={element.trailer_url} frameBorder="0" ></iframe>
-                              <h2 mb-2>{element.name}</h2>
+                               <div className = "container mb-2">
+                                  <h2 >{element.name}</h2>
+                               </div>
                             </div>
                             <div className="content">
                                     <a target = "blank" href = {element.download_url} class="btn btn-success">Download</a>
