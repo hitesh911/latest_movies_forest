cb9b860da711bd3a97d980bc61dee4e29c0b7960
hitesh911 <72139864+hitesh911@users.noreply.github.com>
2021-12-31 16:15:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -164,7 +164,7 @@ class App extends Component {
             return (
                 <>
                 
-                <div className="container row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-1">
+                <div className="container-sm row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-1">
                     {items.map((element) => {
                       return (
                         <>
